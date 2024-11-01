d641168b3fb0bc22a618ba82fe5b38cbd5770242
pollchihuy <poll.chihuy@gmail.com>
2024-11-01 16:14:09 +0700

    SWAGGER CONFIG

--- src/main/java/coid/bcafinance/pcmtugasakhir/security/SecurityConfiguration.java
+++ src/main/java/coid/bcafinance/pcmtugasakhir/security/SecurityConfiguration.java
@@ -52,7 +52,8 @@ public class SecurityConfiguration {
                 csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         request -> request
-                        .requestMatchers("/auth/**",
+                        .requestMatchers(
+                                "/auth/**",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**")
                         .permitAll().anyRequest().authenticated()).
